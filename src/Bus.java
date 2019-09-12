87ba762c9a111f94d2a8b08b59fec4af34eb19d3
jc321565 <rhys.selwyn@my.jcu.edu.au>
2019-09-12 22:31:34 +1000

    Add methods , variables to Bus subclass

--- src/Bus.java
+++ src/Bus.java
@@ -1,2 +1,17 @@
-public class Bus {
+public class Bus extends Vehicle {
+    private int busSpeed = 1;
+    public Bus(double initialX, double initialY,String direction,int currentRoad) {
+        super(initialX, initialY,direction,currentRoad);
+        setSpeed(busSpeed);
+        setLength(getCarLength()*3); // Bus 3 times as long as car
+        setWidth(getLength()*0.5); // Bus width half of length
+    }
+
+    public int getBusSpeed() {
+        return busSpeed;
+    }
+
+    public void setBusSpeed(int busSpeed) {
+        this.busSpeed = busSpeed;
+    }
 }
